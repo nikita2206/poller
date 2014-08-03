95ff4a306c36aaa4e28a25c27983e7cf7cd0ce88
Nikita Nefedov <inefedor@gmail.com>
2014-08-03 10:39:09 +0400

    Add some phpdocs + Change a bit how CallbackTask works

--- Poller/Task/PollerTaskQueue.php
+++ Poller/Task/PollerTaskQueue.php
@@ -11,6 +11,8 @@ interface PollerTaskQueue
     public function isEmpty();
 
     /**
+     * Return new Task if the queue is not empty
+     *
      * @return Task
      */
     public function dequeue();
